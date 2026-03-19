fcf744979832313e1c4a3bb010c44435f6c039b4
Sna9e <151719053+Sna9e@users.noreply.github.com>
2026-03-19 15:15:19 +0800

    Delete agent_app.py

--- agent_app.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Compatibility entrypoint for Streamlit Cloud.
-The Streamlit app should be configured to run this file.
-"""
-
-from streamlit_app import *  # noqa: F401,F403
